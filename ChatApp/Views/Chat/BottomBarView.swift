368419a7242e1d43fdd4fe0bb91cebda3263a3e1
salahbensarar1 <sa.bensarar@gmail.com>
2025-01-04 10:40:25 +0100

    Back End for the chat text using Firestore collections I.

---
+++ ChatApp/Views/Chat/BottomBarView.swift
@@ -0,0 +1,18 @@
+//
+//  BottomBarView.swift
+//  ChatApp
+//
+//  Created by Salah Ben Sarar on 2025. 01. 03..
+//
+
+import SwiftUI
+
+struct BottomBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomBarView()
+}
---
+++ ChatApp/Views/Chat/MessageChatView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageChatView.swift
+//  ChatApp
+//
+//  Created by Salah Ben Sarar on 2025. 01. 03..
+//
+
+import SwiftUI
+
+struct MessageChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageChatView()
+}
